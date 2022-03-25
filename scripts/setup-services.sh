df73a8f7c34754566537ae3286866243bdf7b273
Roy Hyunjin Han <rhh@crosscompute.com>
2022-03-25 19:24:41 -0400

    Remove enable

--- scripts/setup-services.sh
+++ scripts/setup-services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo cp services/* /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable crosscompute-homepage
+# sudo systemctl enable crosscompute-homepage
 sudo systemctl start crosscompute-homepage
