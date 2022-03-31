bf93c72747312e48acd3faed01d7fd200c90f15a
Roy Hyunjin Han <rhh@crosscompute.com>
2022-03-31 12:23:11 -0400

    Fix timer

--- scripts/setup-services.sh
+++ scripts/setup-services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo cp services/* /etc/systemd/system
 sudo systemctl daemon-reload
+# sudo systemctl enable crosscompute-homepage.service
+# sudo systemctl enable crosscompute-homepage.timer
 sudo systemctl start crosscompute-homepage.service
 sudo systemctl start crosscompute-homepage.timer
