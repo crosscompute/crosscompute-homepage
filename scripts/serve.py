96b14f4f6d5f69304274524d367276ba3e0ce4e1
Roy Hyunjin Han <rhh@crosscompute.com>
2022-09-23 01:01:53 -0400

    Use yaml

--- scripts/serve.py
+++ scripts/serve.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from invisibleroads_macros_web.markdown import get_html_from_markdown
+from ruamel.yaml import YAML
 
 
 PORT = 8000
@@ -20,59 +22,13 @@ app.mount('/images', StaticFiles(directory=IMAGES_FOLDER), name='images')
 # app.mount('/styles', StaticFiles(directory=STYLES_FOLDER), name='styles')
 templates = Jinja2Templates(
     directory=TEMPLATES_FOLDER, trim_blocks=True, auto_reload=True)
+configuration = {}
 
 
 @app.get('/', response_class=HTMLResponse)
 async def see_home(request: Request):
-    widgets = [{
-        'name': 'Widget A',
-        'description': 'Description for Widget A',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }, {
-        'name': 'Widget B',
-        'description': 'Description for Widget B',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }, {
-        'name': 'Widget C',
-        'description': 'Description for Widget C',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }, {
-        'name': 'Widget D',
-        'description': 'Description for Widget D',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }]
-    websites = [{
-        'name': 'Website A',
-        'description': 'Description for Website A',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }, {
-        'name': 'Website B',
-        'description': 'Description for Website B',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }, {
-        'name': 'Website C',
-        'description': 'Description for Website C',
-        'link_uri': 'https://www.interestingeverything.com/2013/03/07/tree-frog-amphibian-climber',
-        'image_uri': 'https://i0.wp.com/www.interestingeverything.com/wp-content/uploads/2013/02/red-eyed-tree-frog.png?w=500&ssl=1',
-        'video_uri': 'https://youtu.be/z0-Zi5ngQQw',
-    }]
-    return templates.TemplateResponse('index.html', {
-        'request': request,
-        'widgets': widgets,
-        'websites': websites,
-    })
+    return templates.TemplateResponse('index.html', configuration | {
+        'request': request})
 
 
 @app.get('/favicon.ico')
@@ -81,11 +37,28 @@ async def see_icon():
 
 
 def serve_with(args):
-    uvicorn.run('serve:app', port=args.port, log_level='debug', reload=True)
+    configuration.update(load_configuration(args.configuration_path))
+    uvicorn.run(app, port=args.port, log_level='debug')
+
+
+def load_configuration(path):
+    yaml = YAML()
+    with Path(args.configuration_path).open('rt') as f:
+        c = yaml.load(f)
+    ds = [c]
+    while ds:
+        d = ds.pop()
+        for k, v in d.items():
+            if isinstance(v, dict):
+                ds.append(v)
+            elif k == 'description':
+                d[k] = get_html_from_markdown(v)
+    return c
 
 
 if __name__ == '__main__':
     a = ArgumentParser()
     a.add_argument('--port', metavar='X', default=PORT)
+    a.add_argument('configuration_path')
     args = a.parse_args()
     serve_with(args)
