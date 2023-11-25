8ff3db789b5a6fc66b9a6e324a67ed89178fcf89
Roy Hyunjin Han <rhh@crosscompute.com>
2023-11-25 10:10:38 -0500

    Rename app to tool

--- scripts/setup-packages.sh
+++ scripts/setup-packages.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-command -v python3 || sudo dnf install python3 -y
-
-python3 -m venv \
-    ~/.virtualenvs/crosscompute-homepage
-source \
-    ~/.virtualenvs/crosscompute-homepage/bin/activate
-
 pip install -U \
-    beautifulsoup4 \
     fastapi \
     invisibleroads-macros-process \
     jinja2 \
